da834e39f5088833366b5ad08fb130cffeac5387
gaungbhimahamisesa-debug <gaungbhimahamisesa@gmail.com>
2025-10-24 16:21:00 +0800

    day14

--- Latihan/hari13/react-app-axios/src/pages/CRUDAxios.jsx
+++ Latihan/hari13/react-app-axios/src/pages/CRUDAxios.jsx
@@ -1,48 +1,142 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
-import "./home.css";
+import "../CRUDAxios.css";
+import axios from "axios";
 
 function CRUDAxios() {
   const [data, setData] = useState([]);
+  const [dataCategory, setDataCategory] = useState([]);
+  const [title, setTitle] = useState([]);
+  const [year, setYear] = useState("");
+  const [id, setId] = useState("");
+  const [categoryId] = useState("");
 
   useEffect(() => {
     fetchData();
+    fetchDataMovie();
+    fetchDataCategory();
   }, []);
 
-  const fetchData = () => {
+  const fetchDataMovie = () => {
     axios
       .get("htpp://localhost:3000/api/movie")
       .then((response) => {
-        setData(response.data);
+        setData(response.data.info);
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
+const handleTitleChange = (event) => {
+  setTitle(event.target.value);
+  console.log(title);
+};
+
+const handleYearChange = (event) => {
+  setYear(event.target.value);
+  console.log(year);
+};
+
+const handleCategoryIdChange = (event) => {
+  setCategoryId(event.target.value);
+  console.log(categoryId);
+};
+
+const handleSubmit =async (event) => {
+  event.preventDefault();
+  try {
+    let posting = await axios.post("http://localhost:3000/api/movie", {
+      title: (title),
+      year: Number(year),
+      categoryId: Number(categoryId)
+    });
+    console.log(posting);
+    fetchDataMovie();
+    fetchDataCategory();
+  } catch (err) {
+    alert(err);
+  }
+};
+
+  const handleUpdate = async(id) => {
+      try {
+    let update = await axios.put("http://localhost:3000/api/movie/$(id)", {
+      title: (title),
+      year: Number(year),
+      categoryId: Number(categoryId)
+    });
+    console.log('Item Updated Successfully:', update.data);
+    fetchDataMovie();
+    fetchDataCategory();
+  } catch (err) {
+    alert(err);
+  }
+}
+
+const handleDelete = async(id) => {
+      try {
+    let response = await axios.delete('http://localhost:3000/api/movie/$(id)')
+    console.log('Item Deleted Successfully:', response.data);
+    fetchDataMovie();
+    fetchDataCategory();
+  } catch (err) {
+    alert(err);
+  }
+}
+
+const handleDelete = async(id) => {
+  try {
+    axios.get('http://localhost:3000/api/movie/$(id)'). then ((response) => {
+        let result = response.data.info
+        console.log(result)
+        setTitle[result.title]
+        setYear[result.year]
+        setCategoryId[result.categoryId]
+      })}catch(err) {
+          console.log(err)
+    }
+}
+
   return (
     <>
       <h1> CRUD AXIOS </h1>
+    <fieldset className="fieldset bg-base-300 border-300 rounded-box-w-90 border p-4">
+      <form onSubmit={handleSubmit}>
+        <label className="label" for="title">Movie Title</label>
+        <input className="input"
+          type="text"
+          id="title"
+          name="title"
+          onChange="{handleTitleChange}
+          value=(title)
+          placeholder="Movies"></input>
 
-      {/* <form action="/action_page.php" id="carform">
-  <label for="fname">Firstname:</label>
-  <input type="text" id="fname" name="fname">
+        <label className="label" for="year">year</label>
+        <input className="input"
+          type="text"
+          id="year"
+          name="year"
+          onChange="{handleYearChange}
+          placeholder="Movies Released date"></input>
 
-
-<br>
-
-<label for="cars">Choose a car:</label>
-<select id="cars" name="carlist" form="carform">
-  <option value="volvo">Volvo</option>
-  <option value="saab">Saab</option>
-  <option value="opel">Opel</option>
-  <option value="audi">Audi</option>
-</select>
+        <label for="category">Category</label>
+        <select value=(categoryId) className="select select neutral" id="category" name="category" onChange={handleCategoryChange}>
+                {dataCategory.map((item.index) => {
+                      return (
+                        <option key={index} value={item.id}> {item.name} </option>
+                      )
+                      })}
+        </select>
+                      
 
   <input type="submit"></input>
   </form> */}
 
-      <table>
+        <input type="submit" value="submit" className='btn btn-info' />
+      </form>
+
+      <div className="overflow-x-auto">
+      <table className-='table table-zebra'>
         <thead>
           <tr>
             <th>no</th>
@@ -60,11 +154,14 @@ function CRUDAxios() {
                 <td>{item.title}</td>
                 <td>{item.year}</td>
                 <td>{item.categoryById}</td>
+                <td><input type="submit" value="Ubah" className='btn btn-warning' onClick={()handleEdit./></td>
+                <td><input type="submit" value="Ubah"className='btn btn-error'/></td>
               </tr>
             );
           })}
         </tbody>
       </table>
+      </div>
     </>
   );
 }
